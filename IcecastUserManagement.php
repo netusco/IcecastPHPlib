fcba980185747233ce898d98d7d1d29af86a5b8d
Xesco <xesco@gmail.com>
2013-04-06 13:24:07 +0200

    Improved addMount. Added delMount. Now constructor can have no arguments. Fixed BUG interface name changed

--- IcecastUserManagement.php
+++ IcecastUserManagement.php
@@ -6,14 +6,16 @@
 // by ->(xesco)<-
 
 require_once('./IcecastUMInterface.php');
+define("ICECAST_CONFIG_FILE","/etc/icecast2/icecast.xml");
 
 class IcecastUserManagement implements IcecastUMInterface {
 
     private $xml;
     private $file;
 
-    function __construct($file) 
+    function __construct($file=NULL) 
     {
+        if(!isset($file)) $file = ICECAST_CONFIG_FILE;
         if(file_exists($file)) {
             $this->file = $file;
             $this->xml  = new DomDocument();
@@ -25,14 +27,48 @@ class IcecastUserManagement implements IcecastUMInterface {
             throw new Exception("File $file does not exist or bad xml!");
     }
 
-    public function addMount($mount, $password)
+    public function addMount($mount, $password, $config=NULL)
     {
-        //file_put_contents($this->file, $this->xml->saveXML());
+        // check if mountpoint already exists
+        foreach($this->xml->getElementsByTagName('mount-name') as $mp) 
+        {
+            if(strcmp($mp->nodeValue, $mount)==0)
+                throw new Exception("Mountpoint $mount already exists!");
+        }
+
+        $mp = $this->xml->createElement('mount');
+        $mp->appendChild($this->xml->createElement('mount-name', $mount));
+        $mp->appendChild($this->xml->createElement('password', $password));
+        
+        // custom config
+        if(isset($config)) { 
+            foreach($config as $param => $value) {
+                $mp->appendChild($this->xml->createElement($param, $value));
+            }
+        }
+        // standard config
+        else {
+            $mp->appendChild($this->xml->createElement('max-listeners', '5'));
+            $mp->appendChild($this->xml->createElement('public', '0'));
+            $mp->appendChild($this->xml->createElement('bitrate','160'));
+            $mp->appendChild($this->xml->createElement('type', 'application/mp3'));
+            $mp->appendChild($this->xml->createElement('subtype', 'mp3'));
+            $mp->appendChild($this->xml->createElement('hidden', '1'));
+            $mp->appendChild($this->xml->createElement('burst-on-connect', '0'));
+        }
+        $this->xml->getElementsByTagName('icecast')->item(0)->appendChild($mp);
+        file_put_contents($this->file, $this->xml->saveXML());
     }
     
     public function delMount($mount)
     {
-        //file_put_contents($this->file, $this->xml->saveXML());
+        foreach($this->xml->getElementsByTagName('mount-name') as $mp) 
+        {
+            if(strcmp($mp->nodeValue, $mount)==0) {
+                $mp->parentNode->parentNode->removeChild($mp->parentNode);
+            }
+        }
+        file_put_contents($this->file, $this->xml->saveXML());
     }
     
     public function changeMountPass($mount, $password)
--- test.php
+++ test.php
@@ -2,9 +2,14 @@
 
 require_once 'IcecastUserManagement.php';
 
+
+
+//$icecast = new IcecastUserManagement();
 $icecast = new IcecastUserManagement('./icecast.xml');
 $icecast->setAdminWeb('itworks','itworks');
 $icecast->changeMountPoint('/test.ogg','/itworks.ogg');
 $icecast->changeMountPass('/sdj_stream_133.ogg','new_password');
+$icecast->delMount('/txepe.ogg');
+$icecast->addMount('/prova', 'prova');
 
 ?>
